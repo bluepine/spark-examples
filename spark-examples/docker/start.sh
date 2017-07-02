6b8f99a3269b4006896112d266d8075c7129e9e8
Song Wei <1983.song.wei@gmail.com>
2017-07-02 18:42:09 -0400

    t

--- spark-examples/docker/start.sh
+++ spark-examples/docker/start.sh
@@ -3,4 +3,5 @@ bash ./stop.sh
 sudo rm -rf ./shared/log
 make
 mkdir -p ./shared/log
-docker-compose up
+#docker-compose up
+docker stack deploy --compose-file=docker-compose.yml spark
--- spark-examples/docker/stop.sh
+++ spark-examples/docker/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker-compose stop
-
+#docker-compose stop
+docker stack rm spark
