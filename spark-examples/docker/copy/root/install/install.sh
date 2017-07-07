2a4ec40240416b9b776e35bfb2bc5bc1283c739b
Song Wei <1983.song.wei@gmail.com>
2017-07-07 19:05:50 -0400

    t

--- spark-examples/docker/copy/root/install/install.sh
+++ spark-examples/docker/copy/root/install/install.sh
@@ -6,18 +6,10 @@ mkdir -p /opt
 adduser -S -D -g "" $USER;
 addgroup $USER;
 mkdir -p "${HADOOP_HOME}";
-export ARCHIVE=hadoop-$HADOOP_VERSION.tar.gz;
-export DOWNLOAD_PATH=apache/hadoop/common/hadoop-$HADOOP_VERSION/$ARCHIVE;
-curl -sSL https://mirrors.ocf.berkeley.edu/$DOWNLOAD_PATH | tar -xz -C $HADOOP_HOME --strip-components 1;
-rm -rf $ARCHIVE;
-rm -rf $HADOOP_HOME/share/doc;
 cp -r  /root/install/conf/hadoop/* $HADOOP_CONF_DIR/
 mkdir -p $HDFS_VOL1;
 mkdir -p $HDFS_VOL2;
 
-SPARK_ARCHIVE=https://d3kbcqa49mib13.cloudfront.net/spark-${SPARK_VERSION}.tgz
-mkdir -p $USER_CODE;
-curl -s $SPARK_ARCHIVE | tar -xz -C $USER_CODE && ln -sf $USER_CODE/spark-${SPARK_VERSION} $SPARK_HOME
 mkdir -p $SPARK_HOME/etc/
 cp -r  /root/install/conf/spark/* $SPARK_HOME/etc/
 
