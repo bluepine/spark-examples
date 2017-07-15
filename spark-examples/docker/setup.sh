e28e9155e873523b3156a59aaf66f4f82c03ca9f
Song Wei <1983.song.wei@gmail.com>
2017-07-15 17:29:25 -0400

    conf vol added

--- spark-examples/docker/setup.sh
+++ spark-examples/docker/setup.sh
@@ -10,4 +10,5 @@ export HDFS_VOL1=`extract_env HDFS_VOL1`
 export HDFS_VOL2=`extract_env HDFS_VOL2`
 export USER_TMP=`extract_env USER_TMP`
 export USER_CODE_MNT=`extract_env USER_CODE_MNT`
-
+export SPARK_CONF_DIR=`extract_env SPARK_CONF_DIR`
+export HADOOP_CONF_DIR=`extract_env HADOOP_CONF_DIR`
---
+++ spark-examples/docker/volume/code/lib.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+function check_start {
+    if [ $1 -ne 0 ]; then
+        echo "Failed to start $2: $1"
+        exit $1
+    fi
+}
+
+function check_running {
+    ps aux | grep $1 | grep -q -v grep
+    status=$?
+    if [ $status -ne 0 ]; then
+        echo "$1 stopped. Terminating"
+        exit $status
+    fi
+}
+
+function collect_stdout {
+    #sed -e '/^$/d' |  jq -R . | sed -e 's/^/{"'"$1"'":/' -e 's/$/}/' | stdbuf -oL fluent-bit -f 10 -v -i stdin -o forward://fluentd:24224
+    #    logger --rfc5424 -n fluentd -P 5140 -t $1
+    cat
+}
