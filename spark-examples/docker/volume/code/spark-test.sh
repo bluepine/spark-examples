180b585bb77c2e7a8ccd686f85c2ea91d0b8a081
Song Wei <1983.song.wei@gmail.com>
2017-07-16 19:14:32 -0400

    t

--- spark-examples/docker/volume/code/spark-test.sh
+++ spark-examples/docker/volume/code/spark-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sbin/start-history-server.sh
+bash $SPARK_HOME/sbin/start-history-server.sh
 for class in examples.classification.BikeBuyersDTree examples.classification.BikeBuyersDTreeTuning examples.classification.BikeBuyersRForest examples.classification.BikeBuyersRForestTuning examples.classification.NaiveBayesTfIdfSpamClassifier examples.regression.HousePricesPrediction examples.classification.BikeBuyersDTreeJava examples.classification.BikeBuyersRForestJava examples.classification.NaiveBayesTfIdfSpamClassifierJava examples.regression.HousePricesPredictionJava
 do
     bin/spark-submit --class $class --deploy-mode cluster --master spark://spark-master:6066 /shared/jar/spark-examples-1.0.jar /tmp/data/
