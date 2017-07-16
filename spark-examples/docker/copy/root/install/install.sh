d121e7bd9fb375464591cb0131da52b1ef46926e
Song Wei <1983.song.wei@gmail.com>
2017-07-15 20:13:45 -0400

    t

--- spark-examples/docker/copy/root/install/install.sh
+++ spark-examples/docker/copy/root/install/install.sh
@@ -4,12 +4,11 @@ set -x
 mkdir -p /opt
 adduser -S -D -g "" $USER;
 addgroup $USER;
-cp -r  /root/install/conf/hadoop/* $HADOOP_CONF_DIR/
 mkdir -p $HDFS_VOL1;
 mkdir -p $HDFS_VOL2;
 
 mkdir -p $SPARK_CONF_DIR
-cp -r  /root/install/conf/spark/* $SPARK_CONF_DIR
+echo $USER_HOME
 mkdir $USER_TMP
 #assume all directories owned by $USER are inside $USER_HOME
 chown -R $USER:$USER $USER_HOME;
